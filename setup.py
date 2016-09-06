ccdbf329125ed4bcc3b94dd6ee85e12fec5be77c
Alexander Skovpen <a.n.skovpen@gmail.com>
2016-09-06 17:08:32 +0300

    setup change

--- setup.py
+++ setup.py
@@ -1,12 +1,26 @@
-from distutils.core import setup
+import os
+from setuptools import setup, find_packages
 
+def requirements():
+  """Build the requirements list for this project"""
+  requirements_list = []
+  with open('requirements.txt') as requirements:
+    for install in requirements:
+      requirements_list.append(install.strip())
+  return requirements_list
 setup(name='discord_simple',
-  version='0.0.1.3',
+  version='0.0.1.6',
+  license='LGPLv3',
   description='Discord simple API',
   author='Alexander N. Skovpen',
   author_email='a.n.skovpen@gmail.com',
   url='https://github.com/askovpen/discord_simple/',
-  packages=['discord_simple','discord_simple.models'],
+  install_requires=requirements(),
+  packages=find_packages(exclude=['tests*']),
+  include_package_data=True,
+  extras_require={
+    'json': 'ujson',
+  },
   classifiers=[
     'Development Status :: 3 - Alpha',
     'Intended Audience :: Developers',
